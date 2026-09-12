f24b47e05f48fbcc06af5fe71e07ccd8c2ea186d
chill-guy77 <kiragyawali8@gmail.com>
2026-09-12 19:11:52 +0545

    completed the displaying part and now it displays the output perfectly but now theres one thing remaining like it doesnt remove the questions while pressing the remove button which i will fix now

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const form = document.getElementById('form');
+    const submitt = document.querySelector("#submitt");
     form.addEventListener('submit', (e)=>{
         e.preventDefault();
         formdata = new FormData(form);
@@ -8,6 +9,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         const name = document.querySelector('input[type="text"]').value;
         const limit = document.querySelector('input[type="number"').value;
         const difficulty = formdata.get("difficulty");
+        const result = document.querySelector('#result');
         console.log("limit");
         if(limit>20){
             alert("Please enter a value under 20!");
@@ -31,10 +33,32 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 const questions = result.questions;
                 console.log(questions);
                 console.log(name);
+        document.querySelector("#result").style.visiblity = "visible";
+        document.querySelector("#questions-result").style.visiblity = "visible";
+        const rp = document.querySelector("#result-p")
+        rp.style.display = "revert";
+        rp.innerText = `Here are your questions <b> ${namee} </b>`;
+                const remove = `
+                <button id="remove-output"> Remove </button>
+                `
+    for(let i=0; i<questions.length;i++){
+        const result5 = ` <br>
+        <div class= "actualquestions">
+        ${i + 1}. ${questions[i].question}
+        </div> 
+        `;
+        document.querySelector("#result").insertAdjacentHTML('beforeend',result5);
+    }
+    const result2 = document.querySelector("#result");
+    document.querySelector("#result").insertAdjacentHTML('beforeend',remove);
+    document.querySelector("#remove-output").addEventListener('click',()=>{
+        document.querySelector.querySelector("#result").style.visiblity = "hidden";
+        submitt.disabled = false;
+    })
             }
         })
         .catch(e => console.error(e));
     }
     })
-
+    
 })
