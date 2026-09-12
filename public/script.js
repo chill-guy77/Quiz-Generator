283bfd95eea4a0555005a702f0140a3d28a825f8
chill-guy77 <kiragyawali8@gmail.com>
2026-09-12 14:52:24 +0545

    finally added the questions as questions.json, added the limit and difficulty variable for more and specific question, made the backend more functionable and now the app sends the data and prints in console perfectly, in the next commit i will be looking forward to display the output in the actual page

--- public/script.js
+++ public/script.js
@@ -6,6 +6,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
         formdata = new FormData(form);
         const category = formdata.get('categoryy');
         const name = document.querySelector('input[type="text"]').value;
+        const limit = document.querySelector('input[type="number"').value;
+        const difficulty = formdata.get("difficulty");
+        console.log("limit");
+        if(limit>20){
+            alert("Please enter a value under 20!");
+        }else{
         console.log(name);
         console.log(category);
         fetch('/save',{
@@ -13,16 +19,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
                 name: name,
-                category: category
+                category: category,
+                limit: limit,
+                difficulty: difficulty
             })
         })
         .then(response => response.json())
         .then(result=>{
             if(result.success){
                 const namee = result.name;
+                const questions = result.questions;
+                console.log(questions);
                 console.log(name);
             }
         })
         .catch(e => console.error(e));
+    }
     })
+
 })
--- server.js
+++ server.js
@@ -2,28 +2,32 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
-
+let questions;
+const fs = require('fs');
+const { json } = require('stream/consumers');
+const { diff } = require('util');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// const data = fs.readFileSync('questions.json', utf8);
+// questions = json.parse(data);
+fs.readFile('questions.json', (err, data)=>{
+     questions = JSON.parse(data);
+})
+
 app.post('/save', (req,res)=>{
     const category = req.body.category;
     const name = req.body.name;
-    console.log(name);
-    console.log(category);
-    if(category==='Science'){
-        console.log('Heloo')
-    }else if(category==='Maths'){
-        console.log('Heldoo')
-    }else if(category==='History'){
-        console.log('Heloso')
-    }else if(category==='Geography'){
-        console.log('Heloao')
-    }
+    const limit = req.body.limit;
+    const difficulty = req.body.difficulty;
+    const selected_questions = questions.filter(q => q.category === category);
+    const selected_questions1 = selected_questions.filter(q=>q.difficulty === difficulty).sort(() => Math.random() - 0.5).slice(0, limit);
+
+    console.log(selected_questions1);
     res.json({
         success:true,
-        questions: '<soon the questions are gonna be added in questions.json and i will put up the    questions into here :D',
+        questions: selected_questions1,
         name: name
     })
 })
